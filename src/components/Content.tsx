38bd6a0b05f21b64d0435e2e77495a74e7630ec3
Lucas Inoue <lucas.inoue@rockcontent.com>
2021-03-15 23:20:10 -0300

    refactor to components

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,31 @@
-export function Content() {
+import { MovieCard } from './MovieCard';
+
+
+import { GenreResponseProps, MovieProps } from '../@types/Typings'
+
+import '../styles/content.scss';
+
+
+interface Props {
+  movies: MovieProps[]
+  selectedGenre: GenreResponseProps
+}
+
+export function Content({ movies, selectedGenre }: Props) {
   // Complete aqui
+  return (
+    <div className="container">
+        <header>
+          <span className="category">Categoria:<span> {selectedGenre.title}</span></span>
+        </header>
+
+        <main>
+          <div className="movies-list">
+            {movies.map(movie => (
+              <MovieCard title={movie.Title} poster={movie.Poster} runtime={movie.Runtime} rating={movie.Ratings[0].Value} />
+            ))}
+          </div>
+        </main>
+      </div>
+  )
 }
